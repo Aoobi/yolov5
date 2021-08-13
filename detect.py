198a182a379a05e29f57b6ea389fa8159345fe5e
Aoobi <35079856+Aoobi@users.noreply.github.com>
2021-08-13 10:04:18 +0200

    Update detect.py
    changed colors of detections

--- detect.py
+++ detect.py
@@ -54,7 +54,7 @@ def detect(opt):
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    colors = [[51, 51, 255], [153, 51, 255]]
 
     # Run inference
     if device.type != 'cpu':
